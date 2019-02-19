6d14715b0c345e4a46c500b197e5ae56d782f4ec
wodzynski <macko5395@gmail.com>
2019-02-19 15:17:53 +0100

    Remove jQuerry :)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+// the base length is 20
 var leap = 20;
 var movementCycle;
 var shift = [0, 0];
@@ -8,11 +9,15 @@ var speed = 100;
 var head = [80, 80];
 var snake = [[head[0], head[1]]];
 var trait = [];
+// var clearBoard = new Array(25);
 // var snake = [head];
 
-$(document).ready(function() {
-  init();
-});
+document.addEventListener('DOMContentLoaded', () => init(), false);
+
+// fill clearBoard with undefined
+for (var i = 0; i < clearBoard.length; i++) {
+  clearBoard[i] = new Array(15);
+}
 
 function init() {
   // drawing board and initialize start position of the sznake
@@ -22,13 +27,13 @@ function init() {
   traitGenerator();
 }
 
-$(window).keypress(keyPressHandler);
+document.addEventListener("keydown", keyPressHandler);
 
 function keyPressHandler(e) {
-  var keyCode = e.which;
-  // console.log(e, keyCode, e.which);
+  const keyPressed = e.keyCode;
+  console.log(e, keyPressed, e.keyCode);
   // W key pressed
-  if (keyCode === 119 || keyCode === 87) {
+  if (keyPressed === 119 || keyPressed === 87) {
     // checking if movement occurs horizontally
     if (shift[1] == 0) {
       // if yes, movement modifier is set
@@ -38,14 +43,14 @@ function keyPressHandler(e) {
     }
   }
   // S key pressed
-  if (keyCode === 115 || keyCode === 83) {
+  if (keyPressed === 115 || keyPressed === 83) {
     if (shift[1] == 0) {
       shift = [0, leap];
       movement();
     }
   }
   // A key pressed
-  if (keyCode === 97 || keyCode === 65) {
+  if (keyPressed === 97 || keyPressed === 65) {
     // checking if movement occurs vertically
     if (shift[0] == 0) {
       shift = [-leap, 0];
@@ -53,20 +58,20 @@ function keyPressHandler(e) {
     }
   }
   // D key pressed
-  if (keyCode === 100 || keyCode === 68) {
+  if (keyPressed === 100 || keyPressed === 68) {
     if (shift[0] == 0) {
       shift = [leap, 0];
       movement();
     }
   }
   // Q key pressed
-  if (keyCode == 113 || keyCode === 81) {
+  if (keyPressed == 113 || keyPressed === 81) {
     // stopping movement cycle and clearing shift so movement can start in every dirrection
     clearInterval(movementCycle);
     shift = [0, 0];
+    ctx.clearRect(0, 0, 500, 300);
+    init();
   }
-  // ctx.clearRect(0, 0, 500, 300);
-  // init();
 }
 
 function movement() {
@@ -108,25 +113,13 @@ function move() {
   }
 }
 
+// function trackingBoard() {
+//   head[0]
+// }
+
 function traitGenerator() {
   trait[0] = Math.floor(Math.random() * 25) * 20;
   trait[1] = Math.floor(Math.random() * 15) * 20;
   ctx.fillStyle = "#FF0000";
   ctx.fillRect(trait[0], trait[1], leap, leap);
 }
-
-// function pressedW() {
-//   shift = [0, -10];
-// }
-
-// function pressedS() {
-//   shift = [0, 10];
-// }
-
-// function pressedA() {
-//   shift = [-10, 0];
-// }
-
-// function pressedD() {
-//   shift = [+10, 0];
-// }
